4f4936f5cb18d2cd7039952da9c1bec564af243b
MariamEliasSaoud <lovelymarmoush.saoud14@gmail.com>
2025-07-11 21:07:34 +0300

    commit 5

--- knn.py
+++ knn.py
@@ -95,4 +95,4 @@ port=os.getenv("Port")
 app.run(host='127.0.0.1',port=port)
 # install waitress for run the app cuz flask is in developer mode
 # waitress-serve --listen=127.0.0.1:5000 knn:app (run)
-#pip freeze > requirements.txt will override the req file
+# pip freeze > requirements.txt will override the req file
